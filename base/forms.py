90e28cf043e23664bfb3cce206dec3c3701be25f
luix-091 <luiz.filipe.ctt@gmail.com>
2024-02-15 21:05:18 -0300

    v1.3

--- base/forms.py
+++ base/forms.py
@@ -2,9 +2,11 @@ from django import forms
 from .models import Pessoa, Endereco
 
 class PessoaForm(forms.ModelForm):
+    detalhes = forms.CharField(required=False)
+    necessidades = forms.CharField(required=False)
     class Meta:
         model = Pessoa
-        fields = ['nome', 'cpf', 'data_nasc', 'genero', 'deficiencia', 'necessidades']
+        fields = ['nome', 'cpf', 'data_nasc', 'genero', 'deficiencia','detalhes', 'necessidades', ]
 
 class EnderecoForm(forms.ModelForm):
     class Meta:
--- base/models.py
+++ base/models.py
@@ -7,11 +7,33 @@ class Endereco(models.Model):
     bairro = models.CharField(max_length=100)
     regiao = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.logradouro
+
 class Pessoa(models.Model):
-    nome =  models.CharField(max_length=200)
+    nome =  models.CharField(max_length=200, blank=False, null=False)
     cpf = models.CharField(max_length=20)
     data_nasc = models.DateField()
-    genero = models.CharField(max_length=100)
+    genero = models.CharField(
+        max_length=1,
+        choices=[
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+    ],
+        default='Não informado',
+    )
     enderecos = models.ForeignKey(Endereco, on_delete=models.SET_NULL, null=True, default=None)
-    deficiencia = models.CharField(max_length=300)
-    necessidades = models.CharField(max_length=300)
+    deficiencia = models.CharField(max_length=15,
+                                   choices=[
+                                        ('Física', 'Física'),
+                                        ('Auditiva', 'Auditiva'),
+                                        ('Visual', 'Visual'),
+                                        ('Intelectual', 'Intelectual'),
+                                        ('Psicossocial', 'Psicossocial'),
+                                   ],
+                                   default='Não informada')
+    detalhes = models.CharField(max_length=300, default='Não Fornecido')
+    necessidades = models.CharField(max_length=300, default='Não Fornecida')
+
+    def __str__(self):
+        return self.nome
