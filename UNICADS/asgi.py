23340cb860a4313353f7bd382e8a6974aaf91b05
luix-091 <luiz.filipe.ctt@gmail.com>
2024-02-29 21:17:54 -0300

    v1.9

--- UNICADS/asgi.py
+++ UNICADS/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for UNICAD project.
+ASGI config for UNICADS project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UNICAD.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UNICADS.settings')
 
 application = get_asgi_application()
