2333d357f3697bf314134414276f3bd2ccf844db
luix-091 <luiz.filipe.ctt@gmail.com>
2024-03-10 16:07:08 -0300

    v2.2.2

--- UNICADS/settings.py
+++ UNICADS/settings.py
@@ -121,6 +121,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     path.join(BASE_DIR, 'static'),
 ]
+STATICFILES_DIRS = path.join(BASE_DIR, 'static'),
+STATIC_ROOT = path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
